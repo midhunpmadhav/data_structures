105a2ebcc4107d1d816db54a047d4781f19232a6
midhunpmadhav <midhunpmadhav@gmail.com>
2019-06-21 10:30:18 +0530

    Initial commit

---
+++ graph/dfs.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<list>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+class Graph {
+    private :
+        int v;
+        bool* visited;
+        list<int>* adj;  
+
+    public :
+        Graph(int n) {
+            v = n;
+            visited = new bool[v];
+            adj = new list<int>[v];
+        }
+
+        void add_edge(int x, int y) {
+            adj[x].push_back(y);
+        }
+
+        void rem_edge(int x, int y) {
+            adj[x].remove(y);
+        }
+
+        void clear_visited() {
+            for(int i = 0; i < v; ++i) {
+                visited[i] = false;
+            }
+        }
+
+        void dfs(int start) {
+            stack<int> s;
+
+            clear_visited();
+            s.push(start);
+
+            while(!s.empty()) {
+                start = s.top();
+                s.pop();
+
+                visited[start] = true;
+                cout << start << " ";
+                for(auto i = adj[start].rbegin(); i != adj[start].rend(); ++i) {
+                    if(!visited[*i]) {
+                        s.push(*i);
+                    }
+                }
+            }
+            cout << endl;
+        }
+};
+
+int main() {
+    Graph g(4); 
+    g.add_edge(0, 1); 
+    g.add_edge(0, 2); 
+    g.add_edge(1, 2); 
+    g.add_edge(2, 0); 
+    g.add_edge(2, 3); 
+    g.add_edge(3, 3); 
+
+    cout << "DFS: "; 
+    g.dfs(2);
+}
---
+++ tree/tree_traversal.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+class Node {
+    private:
+        int data;
+
+    public:
+        Node* left;
+        Node* right;
+
+        Node(int i) {
+            data = i;
+            left = right = NULL;
+        }
+
+        static void in_order(Node* root) {
+            if(!root)
+                return;
+
+            in_order(root->left);
+            cout << root->data << " ";
+            in_order(root->right);
+        }
+
+        static void pre_order(Node* root) {
+            if(!root)
+                return;
+
+            cout << root->data << " ";
+            pre_order(root->left);
+            pre_order(root->right);
+        }
+
+        static void post_order(Node* root) {
+            if(!root)
+                return;
+
+            post_order(root->left);
+            post_order(root->right);
+            cout << root->data << " ";
+        }
+
+        static void level_order(Node* root) {
+            queue<Node*> q;
+
+            if(!root)
+                return;
+
+            q.push(root);
+
+            while(!q.empty()) {
+                Node *tmp;
+                
+                tmp = q.front();
+                q.pop();
+
+                cout << tmp->data << " ";
+
+                if(tmp->left)
+                    q.push(tmp->left);
+
+                if(tmp->right)
+                    q.push(tmp->right);
+            }
+            cout << endl;
+        }
+};
+
+int main() { 
+    struct Node *root = new Node(1); 
+
+    root->left = new Node(2); 
+    root->right = new Node(3); 
+    root->left->left = new Node(4); 
+    root->left->right = new Node(5);  
+  
+    cout << "\nPre order traversal of binary tree is \t: "; 
+    Node::pre_order(root);
+    cout << endl;
+  
+    cout << "In order traversal of binary tree is \t: "; 
+    Node::in_order(root);
+    cout << endl;
+  
+    cout << "Post order traversal of binary tree is \t: "; 
+    Node::post_order(root);
+    cout << endl;
+
+    cout << "Level order traversal of binary tree is\t: "; 
+    Node::level_order(root);
+    cout << endl;
+    return 0; 
+} 
