ea92d90264764536136c9beeab4fe16b1477a934
Midhun P <midhunpmadhav@gmail.com>
2019-06-26 09:26:46 +0530

    Update print_pattern.cpp

--- linked_list/print_pattern.cpp
+++ linked_list/print_pattern.cpp
@@ -2,8 +2,7 @@
  * Author      : Midhun P
  * Description : input : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
  *               output: 1, 10, 2, 9, 3, 8, 4, 7, 5, 6
- * Remarks     : Complexity should be O(n).
- *               Floyd’s Cycle finding algorithm
+ * Remarks     : Should be O(n)
  */
 
 #include <iostream>
